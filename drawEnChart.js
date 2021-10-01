d12a24138097356813bfe28faf792b1a7cf74634
SarAhiru <sara.sara1008t@gmail.com>
2021-10-01 22:49:26 +0900

    デジタル教科書csvファイル対応 / 時間経過グラフ

--- drawEnChart.js
+++ drawEnChart.js
@@ -5,15 +5,50 @@ function drawChart(result, dataLength) {
     console.log(result);
     console.log(dataLength);
 
-    var num = [0,0,0,0,0,0,0,0,0,0,0];
+    var num = [0,0,0,0,0,0,0,0]; // 回数
+    var datanum = 0; //配列番号カウント用
+    var ftime = 0; //first time
+    var judge = 0; //初回かどうかを判断
 
     if(dataLength > 0){
-        for(var x = 0; x < dataLength-2 ; ++x){
-            var logNum = result[x][2];
-            //console.log(x + "  " + result[x][2]);
+        for(var x = 1; x < dataLength-1 ; ++x){
+            var logDate = result[x][0]; // date取得
+            var logTarget = result[x][9]; // target取得
 
-            num[logNum] ++;
-            //console.log(num);
+            //console.log(x + "  " + result[x][0]);
+            //console.log(x + "  " + result[x][9]);
+
+            let date = new Date(logDate); //letとvarの違い何？？？
+            console.log(date.toLocaleString());
+            
+            //10秒の始めだけfirst timeを設定
+            if(judge == 0){
+              ftime = new Date(logDate);
+              var time = Math.floor(date.getSeconds() / 10) * 10 ;
+              ftime.setSeconds(time);
+              console.log(ftime.toLocaleString());
+            }
+            judge = 1;
+            
+            console.log("date " + date.getTime());
+            console.log("ftime" + ftime.getTime());
+            console.log(date.getTime() > ftime.getTime());
+            
+            
+            //ここで条件分岐して、一定時間にどのくらい機能を使用したのかを記録させたい
+            while(date.getTime() > ftime.getTime()){ //10秒経過したのかを判断
+                ftime.setSeconds(ftime.getSeconds() + 10);
+                console.log(ftime.toLocaleString());
+                datanum ++;
+                console.log(datanum);
+                console.log("**");
+                
+            }
+            num[datanum-1] ++;
+
+            console.log(num);
+
+            
         }
     }
     //console.log(num);
@@ -21,11 +56,14 @@ function drawChart(result, dataLength) {
 
     var ctx = document.getElementById('myChart').getContext('2d');
     window.myChart = new Chart(ctx, {
-      type: 'bar',
+    　//線グラフ
+      type: 'line',
+      //データ
       data: {
-        labels: ["お絵かきソフト起動","色変更","消しゴム選択","描画開始","描画終了","画像出力","画像保存","画像拡大","画像縮小","描画クリア","線の太さ変更"],
+        labels: ['14:09:10~', '14:09:20~', '14:09:30~', '14:09:40~', '14:09:50~', '14:10:00~', '14:10:10', '14:10:20'],
+        //データセット
         datasets: [{
-          label: '# of Votes',
+          label: '操作回数',
           data: num,
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor:'rgba(255, 99, 132, 1)',
