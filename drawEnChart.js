634f4f5d40231ff4fa029a2769c9df7fb16ab332
SarAhiru <sara.sara1008t@gmail.com>
2021-10-04 00:43:09 +0900

    var→letに。データセットの回数、変動可能に。

--- drawEnChart.js
+++ drawEnChart.js
@@ -5,35 +5,36 @@ function drawChart(result, dataLength) {
     console.log(result);
     console.log(dataLength);
 
-    var lavel = [];
-    var num = [0,0,0,0,0,0,0,0]; // 回数
+    let lavel = []; //グラフ横軸ラベル
+    let num = []; // 操作回数
+    //let num = [0,0,0,0,0,0,0,0]; // 操作回数
 
-    var datanum = 0; //配列番号カウント用
-    var ftime = 0; //first time
-    var judge = 0; //初回かどうかを判断
+    let datanum = 0; //配列番号カウント用
+    let ftime = 0; //first time
+    let judge = 0; //初回かどうかを判断
 
-    var flavel = 0; //グラフの軸ラベル用
-    var fsecond = 0;
-    var fminute = 0;
-    var fhour = 0;
+    let flavel = 0; //グラフの軸ラベル用
+    let fsecond = 0;
+    let fminute = 0;
+    let fhour = 0;
 
     if(dataLength > 0){
-        for(var x = 1; x < dataLength-1 ; ++x){
-            var logDate = result[x][0]; // date取得
-            var logTarget = result[x][9]; // target取得
+        for(let x = 1; x < dataLength-1 ; ++x){
+            let logDate = result[x][0]; // date取得
+            let logTarget = result[x][9]; // target取得
 
             //console.log(x + "  " + result[x][0]);
             //console.log(x + "  " + result[x][9]);
 
-            let date = new Date(logDate); //letとvarの違い何？？？
+            let date = new Date(logDate); //letとletの違い何？？？
             console.log(date.toLocaleString());
             
             //10秒の始めだけfirst timeを設定
             if(judge == 0){
               ftime = new Date(logDate);
-              var time = Math.floor(date.getSeconds() / 10) * 10 ;
+              let time = Math.floor(date.getSeconds() / 10) * 10 ;
               ftime.setSeconds(time);
-              console.log(ftime.toLocaleString());
+              //console.log(ftime.toLocaleString());
 
               //グラフの軸ラベル設定(最初)
               fsecond = logDate.substr( 18 );
@@ -42,20 +43,14 @@ function drawChart(result, dataLength) {
               
               fsecond = Math.floor(fsecond / 10) *10;
               
-              console.log(fsecond);
-              console.log(fminute);
-              console.log(fhour);
               //文字を数字に直す変換が必要！！！！！
               fminute = parseInt(fminute);
               fhour = parseInt(fhour);
-              console.log(fminute);
-              console.log(fhour);
-
 
               if(fsecond == 0 || fminute < 10 || fhour < 10){
-                var second2 = fsecond;
-                var minute2 = fminute;
-                var hour2 = fhour;
+                let second2 = fsecond;
+                let minute2 = fminute;
+                let hour2 = fhour;
                 if(fsecond == 0){
                   second2 = "00";
                 }
@@ -63,7 +58,7 @@ function drawChart(result, dataLength) {
                   minute2 = "0" + String(fminute);
                 }
                 if(fhour < 10){
-                  var minute2 = "0" + String(fminute);
+                  hour2 = "0" + String(fhour);
                 }
                 lavel[0] = hour2 + ":" + minute2 + ":" + second2 ;
               }
@@ -74,18 +69,12 @@ function drawChart(result, dataLength) {
             }
             judge = 1;
             
-            //console.log("date " + date.getTime());
-            //console.log("ftime" + ftime.getTime());
-            //console.log(date.getTime() > ftime.getTime());
-            
-            
             //ここで条件分岐して、一定時間にどのくらい機能を使用したのかを記録させたい
             while(date.getTime() > ftime.getTime()){ //10秒経過したのかを判断
                 ftime.setSeconds(ftime.getSeconds() + 10);
                 console.log(ftime.toLocaleString());
                 
                 //グラフの軸ラベル設定
-                console.log(datanum);
                 fsecond = fsecond + 10;
                 if(fsecond >= 60){
                   fsecond = 00;
@@ -98,9 +87,9 @@ function drawChart(result, dataLength) {
                 }
 
                 if(fsecond == 0 || fminute < 10 || fhour < 10){
-                  var second2 = fsecond;
-                  var minute2 = fminute;
-                  var hour2 = fhour;
+                  let second2 = fsecond;
+                  let minute2 = fminute;
+                  let hour2 = fhour;
                   if(fsecond == 0){
                     second2 = "00";
                   }
@@ -108,39 +97,33 @@ function drawChart(result, dataLength) {
                     minute2 = "0" + String(fminute);
                   }
                   if(fhour < 10){
-                    var minute2 = "0" + String(fminute);
+                    hour2 = "0" + String(fhour);
                   }
                   lavel[datanum + 1] = hour2 + ":" + minute2 + ":" + second2 ;
                 }
                 else{
                   lavel[datanum + 1] = fhour + ":" + fminute + ":" + fsecond ;
                 }
-
-                console.log(fsecond);
-                console.log(fminute);
-                console.log(fhour);
-                //lavel[datanum+1] = flavel + fsecond;
-                
                 console.log(lavel);
-                //lavel[datanum+1] = logDate.substr( 12 );
-                //console.log(lavel);
 
                 datanum ++;
-                console.log(datanum);
                 console.log("**");
+                
+                if(num[datanum-1] == null){
+                  num[datanum-1] =0;
+                }
+                
                 
             }
             num[datanum-1] ++;
+            num[datanum] =0;
 
             console.log(num);
-
-            
         }
     }
-    //console.log(num);
     
 
-    var ctx = document.getElementById('myChart').getContext('2d');
+    let ctx = document.getElementById('myChart').getContext('2d');
     window.myChart = new Chart(ctx, {
     　//線グラフ
       type: 'line',
@@ -167,5 +150,5 @@ function drawChart(result, dataLength) {
         }
       }
     });
-    console.log(num);
+    //console.log(num);
 }
