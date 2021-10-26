8feeb322bf17287124c24964c1ab8f0716f69cc8
SarAhiru <sara.sara1008t@gmail.com>
2021-10-26 16:38:03 +0900

    人数カウント棒グラフを作成し始めた

--- drawEnChart2.js
+++ drawEnChart2.js
@@ -1,23 +1,20 @@
-//学習者用デジタル教科書　英語　ログ　グラフ生成　[機能ごと]
+//学習者用デジタル教科書　英語　ログ　人数合計グラフ生成　[全体]
 
-function drawChartTarget(result, dataLength) {
+function drawChart2(result, dataLength) {
 
-    console.log(result);
-    console.log(dataLength);
+    //console.log(result);
+    //console.log(dataLength);
 
     let lavel = []; //グラフ横軸ラベル
-    let indexCount = []; //目次を見た回数
-    let cpCount = []; //ページ移動の回数
-    let audioCount = []; //オーディオの回数
-    let penColorCount = []; //ペン色変更回数
-    let penCount = []; //ペン選択回数
-
+    let num = []; // 操作回数
+    //let num = [0,0,0,0,0,0,0,0]; // 操作回数
 
     let datanum = 0; //配列番号カウント用
     let ftime = 0; //first time
     let judge = 0; //初回かどうかを判断
- 
-    let fsecond = 0; //グラフの軸ラベル用
+
+    let flavel = 0; //グラフの軸ラベル用
+    let fsecond = 0;
     let fminute = 0;
     let fhour = 0;
 
@@ -29,19 +26,9 @@ function drawChartTarget(result, dataLength) {
             //console.log(x + "  " + result[x][0]);
             //console.log(x + "  " + result[x][9]);
 
-            let date = new Date(logDate); //logDateをDate型に直す
+            let date = new Date(logDate); //letとletの違い何？？？
             //console.log(date.toLocaleString());
-
-            //操作のカウントをしたい
-            //console.log(logTarget);
-            //console.log('index_page');
-            //console.log(typeof(logTarget));
-            //console.log(typeof('index_page'));
-
-            //console.log(logTarget == 'index_page');
             
-
-
             //10秒の始めだけfirst timeを設定
             if(judge == 0){
               ftime = new Date(logDate);
@@ -122,106 +109,36 @@ function drawChartTarget(result, dataLength) {
                 datanum ++;
                 //console.log("**");
                 
-                //nullのときにはデータを0に設定
-                if(indexCount[datanum-1] == null){
-                  indexCount[datanum-1] =0;
+                if(num[datanum-1] == null){
+                  num[datanum-1] =0;
                 }
-                if(cpCount[datanum-1] == null){
-                  cpCount[datanum-1] =0;
-                }
-                if(audioCount[datanum-1] == null){
-                  audioCount[datanum-1] =0;
-                }
-                if(penColorCount[datanum-1] == null){
-                  penColorCount[datanum-1] =0;
-                }
-                if(penCount[datanum-1] == null){
-                  penCount[datanum-1] =0;
-                }
+                
+                
             }
-            //indexCount[datanum-1] ++;
-            //indexCount[datanum] =0;
+            num[datanum-1] ++;
+            num[datanum] =0;
 
-            if(logTarget.includes('index')){ // indexの文字を含んでいるのかどうか
-              //console.log(logTarget);
-              indexCount[datanum-1] ++;
-            }
-            else if(logTarget.includes('cp')){
-              //console.log(logTarget);
-              cpCount[datanum-1] ++;
-            }
-            else if(logTarget.includes('audio')){
-              //console.log(logTarget);
-              audioCount[datanum-1] ++;
-            }
-            else if(logTarget.includes('color')){
-              //console.log(logTarget);
-              penColorCount[datanum-1] ++;
-            }
-            else if(logTarget.includes('true') || logTarget.includes('penType')){
-              console.log(logTarget);
-              penCount[datanum-1] ++;
-            }
-
-            //console.log("***");
-            //console.log(logTarget.includes('cp'));
-
-            indexCount[datanum] =0;
-            cpCount[datanum] =0;
-            audioCount[datanum] =0;
-            penColorCount[datanum] =0;
-            penCount[datanum] =0;
-
-            console.log(penCount);
+            //console.log(num);
         }
     }
     
 
-    let ctx = document.getElementById('myChart1').getContext('2d');
-    window.myChart1 = new Chart(ctx, {
+    let ctx = document.getElementById('myChart2').getContext('2d');
+    window.myChart2 = new Chart(ctx, {
     　//線グラフ
-      type: 'line',
+      type: 'bar',
       //データ
       data: {
         labels: lavel,
         //labels: ['14:09:10~', '14:09:20~', '14:09:30~', '14:09:40~', '14:09:50~', '14:10:00~', '14:10:10', '14:10:20'],
         //データセット
         datasets: [{
-          label: '目次閲覧',
-          data: indexCount,
-          backgroundColor: 'rgba(127, 0, 255, 0.2)',
-          borderColor:'rgba(127, 0, 255, 2)',
+          label: '操作回数',
+          data: num,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor:'rgba(255, 99, 132, 1)',
           borderWidth: 1
-        },
-        {
-          label: 'ページ移動',
-          data: cpCount,
-          backgroundColor: 'rgba(0, 128, 255, 0.2)',
-          borderColor:'rgba(0, 128, 255, 1)',
-          borderWidth: 1
-        },
-        {
-          label: 'オーディオ',
-          data: audioCount,
-          backgroundColor: 'rgba(0, 255, 0, 0.2)',
-          borderColor:'rgba(0, 255, 0, 1)',
-          borderWidth: 1
-        },
-        {
-          label: 'ペン色変更',
-          data: penColorCount,
-          backgroundColor: 'rgba(255, 255, 0, 0.2)',
-          borderColor:'rgba(255, 128, 0, 1)',
-          borderWidth: 1
-        },
-        {
-          label: 'ペン選択',
-          data: penCount,
-          backgroundColor: 'rgba(255, 0, 0, 0.2)',
-          borderColor:'rgba(255, 0, 0, 1)',
-          borderWidth: 1
-        }
-        ]
+        }]
       },
       options: {
         scales: {
