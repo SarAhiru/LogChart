67a784cfe6f64885b21c40c39aebd630254d1be9
SarAhiru <sara.sara1008t@gmail.com>
2021-12-21 22:29:43 +0900

    オーディオの表作成　基盤

--- EnglishSound.js
+++ EnglishSound.js
@@ -84,65 +84,33 @@ function table(result, dataLength){
             }
 
         }
-
-        for(let a = 0; a < member.length ; a++){
-            if(second[a] == 0){
-                second[a] = "00";
-            }
-            if(minute[a] < 10){
-                minute[a] = "0" + String(minute[a]);
-            }
-            audioData[a][2] =  minute[a] + ":" + second[a] ;
-            console.log(audioData);
-            console.log(minute);
-            console.log(second);
-        }
     }
 
-    let newRow = table.insertRow();
+    //再生時間の修正
+    for(let a = 0; a < member.length ; a++){
+        if(second[a] == 0){
+            second[a] = "00";
+        }
+        if(minute[a] < 10){
+            minute[a] = "0" + String(minute[a]);
+        }
+        audioData[a][2] =  minute[a] + ":" + second[a] ;
+        console.log(audioData);
+        console.log(minute);
+        console.log(second);
+    }
 
-    let newCell = newRow.insertCell();
-    let newText = document.createTextNode('山田');
-    newCell.appendChild(newText);
+    //テーブルのセルを１行目以外削除する
+    while( table.rows[ 1 ] ) table.deleteRow( 1 );
 
-    newCell = newRow.insertCell();
-    newText = document.createTextNode(18);
-    newCell.appendChild(newText);
-    //console.log("Js途中まで");
-}
+    //テーブルのセルを追加する
+    for(let x = 0; x < member.length ; x++){
+        let newRow = table.insertRow();
 
-/*
-// 表の動的作成
-function makeTable(data, tableId){
-    // 表の作成開始
-    var rows=[];
-    var table = document.createElement("table");
-
-    // 表に2次元配列の要素を格納
-    for(i = 0; i < data.length; i++){
-        rows.push(table.insertRow(-1));  // 行の追加
-        for(j = 0; j < data[0].length; j++){
-            cell=rows[i].insertCell(-1);
-            cell.appendChild(document.createTextNode(data[i][j]));
-            // 背景色の設定
-            if(i==0){
-                cell.style.backgroundColor = "#bbb"; // ヘッダ行
-            }else{
-                cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
-            }
+        for(let y = 0; y < 4 ; y++){
+            let newCell = newRow.insertCell();
+            let newText = document.createTextNode(audioData[x][y]);
+            newCell.appendChild(newText);
         }
     }
-    // 指定したdiv要素に表を加える
-    document.getElementById(tableId).appendChild(table);
 }
-window.onload = function(){ 
-// 表のデータ
-var data = [[11, 12, 13],
-            [21, 22, 23],
-            [31, 32, 33],
-            [41, 42, 43]];
-
-// 表の動的作成
-makeTable(data,"table");
-};
-*/
