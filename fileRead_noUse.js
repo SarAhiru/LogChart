35dba543a53b08815136bb1e332349c4f69c986f
SarAhiru <sara.sara1008t@gmail.com>
2022-04-18 17:00:45 +0900

    Ver5.3　firebase_Set実装完了

--- fileRead_noUse.js
+++ fileRead_noUse.js
@@ -1,3 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request', getCss);
+server.listen(ポート番号, ホスト名);
+console.log('Server running …');
+
+function getCss(req, res) {
+  var url = req.url;
+  if ('/' == url) {
+    fs.readFile('./css.html', 'UTF-8', function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  } else if ('./css/test.css' == url) {
+    fs.readFile('./css/test.css', 'UTF-8', function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }
+}
+
+
 
 var admin = require("firebase-admin");
 
