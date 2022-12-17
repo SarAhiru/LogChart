f44a3887fe2e2ae8b778d4489d34ebbc7449bf9a
SarAhiru <sara.sara1008t@gmail.com>
2022-12-17 20:38:18 +0900

    確認用

--- dateselector.js
+++ dateselector.js
@@ -1,4 +1,12 @@
 function dateselect(result) {
+    const element = document.getElementById("dateselect");
+    // console.log(element.options.length)
+    for (let x = element.options.length-1; x >= 0; x--) {
+        // console.log(x)
+        element.remove(x);
+    }
+    // console.log(element.options.length)
+
     // console.log(result);
     let datelist = [];
 
@@ -9,19 +17,23 @@ function dateselect(result) {
 
             if (datelist.indexOf(date) >= 0) {
             } else {
-                // selectタグを取得する
-                var select = document.getElementById("dateselect");
-                // optionタグを作成する
-                var option = document.createElement("option");
-                // optionタグのテキストをdateに設定する
-                option.textContent = date;
-                // optionタグのvalueを設定する
-                option.value = date;
-                // option.value = datelist.length;
-                // selectタグの子要素にoptionタグを追加する
-                select.appendChild(option);
-                datelist[datelist.length] = date;
-                // console.log(datelist);
+                if(date == "date"){
+
+                }else{
+                    // selectタグを取得する
+                    var select = document.getElementById("dateselect");
+                    // optionタグを作成する
+                    var option = document.createElement("option");
+                    // optionタグのテキストをdateに設定する
+                    option.textContent = date;
+                    // optionタグのvalueを設定する
+                    option.value = date;
+                    // option.value = datelist.length;
+                    // selectタグの子要素にoptionタグを追加する
+                    select.appendChild(option);
+                    datelist[datelist.length] = date;
+                    // console.log(datelist);
+                }
             }
         }
     }
